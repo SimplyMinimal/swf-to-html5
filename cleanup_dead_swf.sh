9c5c645c3369262e0fcb7074bf80b2edc83b60ed
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2020-05-21 00:19:21 -0400

    add comments to cleanup_dead_swf.sh

--- cleanup_dead_swf.sh
+++ cleanup_dead_swf.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script is optional but useful to clean up a batch of dead SWF files
+# REQUIRES IMAGEMAGICK to be installed in order to use 'identify'
+
 PATH_TO_ASSETS=`pwd`/assets
 echo "ASSETS: ${PATH_TO_ASSETS}"
 
@@ -8,6 +11,8 @@ for f in ${PATH_TO_ASSETS}/*.png; do
     
     echo "Validating image for ${PNG_BASENAME}..."
     
+    # Assumes that the number of colors found equal to 1 would mean that the thumbnail is all white or black
+    # This most likely means it's a dead SWF so let's remove it
     if [[ `identify -verbose ./assets/${PNG_BASENAME}|grep "Colors:"|cut -f 2 -d ":"` -eq 1 ]]; then
         echo "Empty image found...removing SWF and PNG"
         rm ./assets/${PNG_TITLE}*
